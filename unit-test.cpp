3b2f9038a527e82bcd9bb4a9241b563d72821f76
Tharci <tmarcellt.2000@gmail.com>
2021-05-06 22:14:02 +0200

    Created unit tests in order to ensure that the compression algorithm works as intended.

--- unit-test.cpp
+++ unit-test.cpp
@@ -1,3 +1,66 @@
+#define CATCH_CONFIG_MAIN
 #include "catch.hpp"
+
+extern "C" {
+    #include "encoded.h"
+};
+
+
+static bool pairEquals(Pair p1, Pair p2) {
+    return p1.c == p2.c && p1.n == p2.n;
+}
+
+TEST_CASE( "encoding and decoding works as intended", "[encode-decode]" ) {
+    SECTION("length of the string is 0") {
+        const char* str = "";
+        Encoded* encoded = makeEncoded(str);
+        REQUIRE(encoded->length == 0);
+
+        char decoded[10];
+        decodeSequence(decoded, 10, encoded);
+        REQUIRE(strcmp(decoded, str) == 0);
+
+        deleteEncoded(encoded);
+    }
+
+    SECTION("length of the string is 0") {
+        auto str = "a";
+        Encoded* encoded = makeEncoded(str);
+        REQUIRE((encoded->length == 1 && pairEquals(encoded->pairSequence[0], Pair {'a', 1})));
+
+        char decoded[10];
+        decodeSequence(decoded, 10, encoded);
+        REQUIRE(strcmp(decoded, str) == 0);
+
+        deleteEncoded(encoded);
+    }
+
+    SECTION("same characters next to each other") {
+        auto str = "aaa";
+        Encoded* encoded = makeEncoded(str);
+        REQUIRE((encoded->length == 1 && pairEquals(encoded->pairSequence[0], Pair {'a', 3})));
+
+        char decoded[10];
+        decodeSequence(decoded, 10, encoded);
+        REQUIRE(strcmp(decoded, str) == 0);
+
+        deleteEncoded(encoded);
+    }
+
+    SECTION("different characters next to each other") {
+        auto str = "abc";
+        Encoded* encoded = makeEncoded(str);
+        REQUIRE((encoded->length == 3 &&
+                pairEquals(encoded->pairSequence[0], Pair {'a', 1}) &&
+                pairEquals(encoded->pairSequence[1], Pair {'b', 1}) &&
+                pairEquals(encoded->pairSequence[2], Pair {'c', 1})));
+
+        char decoded[10];
+        decodeSequence(decoded, 10, encoded);
+        REQUIRE(strcmp(decoded, str) == 0);
+
+        deleteEncoded(encoded);
+    }
+}
 
 
