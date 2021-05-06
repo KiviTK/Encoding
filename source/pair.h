e0ee80af9c9354b5c6c8383da47c4325abb859fc
Tharci <tmarcellt.2000@gmail.com>
2021-05-06 20:57:59 +0200

    Declared Pair struct in source/pair.h

---
+++ source/pair.h
@@ -0,0 +1,15 @@
+#ifndef PAIR_H
+#define PAIR_H
+
+/*
+ * @brief   A Pair represents an element of an encoded string in the Encoded struct.
+ *          This is a pair containing a character and an integer.
+ * @field c The character that is encoded.
+ * @field n The count of the character.
+ */
+typedef sturct {
+    char c;
+    int n;
+} Pair;
+
+#endif
